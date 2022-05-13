c265426207e6ed6417a8a6565fe98ef44ed7fcf5
blackadder-git <blackadder-git@email.com>
2022-05-13 12:21:19 -0700

    modified endpoints

--- routes/contacts.js
+++ routes/contacts.js
@@ -11,12 +11,12 @@ router.get('/', contactsController.getAll); // contacts/
 router.get('/:id', contactsController.getSingle); // contacts/1234567890
 
 // Create a POST route to create a new contact. All fields are required. Return the new contact id in the response body.
-router.post('/create', contactsController.createContact); // contacts/create/???
+router.post('/', contactsController.createContact); // contacts/create/???
 
 // Create a PUT route to update a contact. This route should allow for a url similar to this: api-url-path/contacts/id-to-modify. Return an http status code representing the successful completion of the request.
-router.put('/update/:id', contactsController.updateContact); // contacts/update/1234567890/???
+router.put('/:id', contactsController.updateContact); // contacts/update/1234567890/???
 
 // Create a DELETE route to delete a contact. Return an http status code representing the successful completion of the request.
-router.delete('/delete/:id', contactsController.deleteContact); // contacts/delete/1234567890/???
+router.delete('/:id', contactsController.deleteContact); // contacts/delete/1234567890/???
 
 module.exports = router;
