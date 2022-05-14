85daad0d70bd573da322dc19131e06ea170d0df1
blackadder-git <blackadder-git@email.com>
2022-05-14 02:00:42 -0700

    route testing for frontend

--- server.js
+++ server.js
@@ -19,12 +19,14 @@ app
 .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*'); // CORS
     // needed for react frontend to work
+    /*
     res.setHeader(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
     );
     //res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    */
     next();
 })
 .use('/', require('./routes')); // goto routes
